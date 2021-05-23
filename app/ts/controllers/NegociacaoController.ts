9551b9aec895dfe1c8ed93f841a64b50ff7dcfa5
fej0se <80367187+fej0se@users.noreply.github.com>
2021-05-22 22:05:42 -0300

    view

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -2,20 +2,29 @@ class NegociacaoController {
   private _inputData: HTMLInputElement;
   private _inputQuantidade: HTMLInputElement;
   private _inputValor: HTMLInputElement;
+  private _negociacoes: Negociacoes = new Negociacoes();
+  private _negociacoesView = new NegociacoesView('#negociacoesView');
 
   constructor() {
     this._inputData = <HTMLInputElement>document.querySelector('#data');
     this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
     this._inputValor = <HTMLInputElement>document.querySelector('#valor');
+
+    //render view
+    this._negociacoesView.update(this._negociacoes);
+
   }
 
   adiciona(event: Event) {
     event.preventDefault();
     const negociacao = new Negociacao(
       new Date(`${this._inputData.value}T00:00`), //ou new Date(this._inputData.value.replace(/-/g, '/'))
-      parseInt(this._inputQuantidade.value), 
-      parseFloat(this._inputValor.value))
-      console.log(this._inputData)
-    console.log(negociacao)
+      parseInt(this._inputQuantidade.value),
+      parseFloat(this._inputValor.value)
+      );
+      this._negociacoes.adiciona(negociacao)
+
+      //update view
+      this._negociacoesView.update(this._negociacoes);
   }
 }
